c6c46c75a216274fd01a9c5d782d0e28858790e1
szczek <ignacy.rolinski@gmail.com>
2024-11-30 20:49:52 +0100

    dodaj nowe algorytmy

--- algorithms/G3_calkowanie_monteCarlo/calkowanie_monteCarlo/main.cpp
+++ algorithms/G3_calkowanie_monteCarlo/calkowanie_monteCarlo/main.cpp
@@ -1,14 +1,35 @@
-//
-//  main.cpp
-//  calkowanie_monteCarlo
-//
-//  Created by Ignacy Roliński on 17/11/2024.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+double f(double x){
+    return x * x * x;
+};
+
+double monteCarloEstimate(double a, double b, int n){
+    srand(time(NULL));
+    double suma = 0;
+    for (int i = 1; i <= n; i++){
+        //wylosuj liczbe
+        double los = a + ((double)rand()/RAND_MAX * (b-a));
+        //wex wartosc funkcji
+        double wartoscFunkcjiOdX = f(los);
+        //dodaj f(x) do sumy
+        suma = suma + wartoscFunkcjiOdX;
+    };
+    
+    double calka = (b-a) * suma / n;
+    return calka;
+};
+
+int main(){
+    
+    double a = 1;
+    double b = 2;
+    int n = 1000;
+    //wynik 3.75?? powinien wyjsc
+    
+    double wynik = monteCarloEstimate(a, b, n);
+    
+    std::cout << wynik;
     return 0;
-}
+};
+    
--- algorithms/G4_quicksort/G4_quicksort/main.cpp
+++ algorithms/G4_quicksort/G4_quicksort/main.cpp
@@ -1,10 +1,60 @@
 #include <iostream>
 
+int tablica[10] = {5, 242, 44, 998, 997, 444, 123, 83, 123, 70};
+
+void pokazTablice(int *array, int n)
+{
+
+    std::cout << "[";
+    for (int i = 0; i < n; ++i){
+        std::cout << array[i];
+        i != n-1 && std::cout << ", ";
+    }
+    std::cout << "] \n";
+}
+
+
+
+void quicksort (int *tablica, int indexPoczatekTablicy, int indexKoniecTablicy){
+    
+    int i = indexPoczatekTablicy;
+    int j = indexKoniecTablicy;
+    int srodek = tablica[(i + j) /2]; // wyznacz środek tablicy
+    int temp; // zarezerwuj miejsce na jedną liczbę do zapamiętania
+    
+    //dopilnuj, żeby w czasie pętli nie wyjść poza wyznaczony argumentami obszar
+    while (i <= j){
+        //powiększaj lewą stronę aż znajdzie się przed środkiem
+        while(tablica[i] < srodek){
+            i++;
+        };
+        //zmniejszaj prawą stronę aż znajdzie się przed środkiem
+        while(tablica[j] > srodek){
+            j--;
+        };
+        
+        //upewnij się, że i <= j zanim wykonasz kolejne operacje
+        if(i <= j){
+            temp = tablica[i];
+            tablica[i] = tablica[j];
+            tablica[j] = temp;
+            i++;
+            j--;
+        };
+    };
+    
+    if (j > indexPoczatekTablicy){
+        quicksort(tablica, indexPoczatekTablicy, j);
+    };
+    if (i < indexKoniecTablicy){
+        quicksort(tablica, i, indexKoniecTablicy);
+    };
+};
+
 int main() {
+    int dlugoscTablicy = sizeof(tablica)/sizeof(tablica[0]);
+    quicksort(tablica, 0, dlugoscTablicy - 1);
     
-    int tablica[10] = {5, 242, 44, 998, 997, 444, 123, 83, 123, 70};
-    
-    
- 
+    pokazTablice(tablica, dlugoscTablicy);
     return 0;
 };
